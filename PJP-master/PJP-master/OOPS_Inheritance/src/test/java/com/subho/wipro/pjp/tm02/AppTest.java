153a6878919651377369538ff63a24d63bc40282
Yuvaranjani123 <47520340+Yuvaranjani123@users.noreply.github.com>
2022-12-05 12:08:56 +0530

    File Creation

---
+++ PJP-master/PJP-master/OOPS_Inheritance/src/test/java/com/subho/wipro/pjp/tm02/AppTest.java
@@ -0,0 +1,14 @@
+package com.subho.wipro.pjp.tm02;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class AppTest {
+
+	@Test
+	void testMain() {
+		System.out.println("Test Started running!");
+	}
+
+}
